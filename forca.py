3a3b7af3ff69639db12263cfe6fcc2aa40d41113
Jonas Nunes <70301882+jonasnunes@users.noreply.github.com>
2023-02-09 15:39:04 -0300

    Update forca.py
    mensagem personalizada para o ganhador e perdedor

--- forca.py
+++ forca.py
@@ -114,12 +114,40 @@ def substituindo_letras(index, chute, letra, palavra_secreta, letras_acertadas):
 
 
 def mensagem_vencedor():
-    print('\nParabéns... Você ganhou o jogo!')
+
+    print("\nParabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
 
 
 def mensagem_perdedor(palavra_secreta):
-        print('\nEsgotaram-se suas tentativas... Você perdeu!')
-        print('\nA palavra secreta era {}'.format(' '.join(palavra_secreta)))
+        
+    print("\nPoxa, você foi enforcado!")
+    print("A palavra era {}".format(' '.join(palavra_secreta)))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
 
 
 if __name__ == '__main__':
